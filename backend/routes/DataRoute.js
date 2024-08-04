3baa9b3ccc33d818529f1f62f04559102ff7c2e5
Recep Kurban <kurbanrecep@gmail.com>
2024-08-04 21:18:21 +0300

    remove data route

--- backend/routes/DataRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as controller from '../controllers/DataController.js';
-
-const router = express.Router();
-
-router.get("/data", controller.getAllData);
-
-export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import connectDB from './config/db.js';
 import baseRoute from './routes/BaseRoute.js'
 import authRoute from './routes/AuthRoute.js'
-import dataRoute from './routes/DataRoute.js'
 
 const app = express();
 const port = 3000;
@@ -10,7 +9,6 @@ app.use(express.json());
 
 app.use("/", baseRoute);
 app.use("/api/v1/auth/", authRoute);
-app.use("/api/v1/data/", dataRoute);
 
 try {
     await connectDB();
